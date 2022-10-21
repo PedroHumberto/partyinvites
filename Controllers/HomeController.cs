1ea2cd7492be1c153f9ec68a2ccd0752ab054fca
Pedro Humberto <pedrohlucape@gmail.com>
2022-10-20 21:42:55 -0300

    first commit, learning how to use view

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index(){
+        return View();
+    }
+
+    //Second Action view
+    [HttpGet]
+    public ViewResult RsvpForm(){
+        return View();
+    }
+
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse){
+        Repository.AddResponse(guestResponse);
+        return View("Thanks", guestResponse);
+    }
+
+    public ViewResult ListResponce(){
+        return View(Repository.Responses.Where(r => r.WillAttend == true));
+    }
+
+}
